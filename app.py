012869f6c7393b1edf354655ead0479202729fde
rubensval25 <rubensval25@gmail.com>
2024-01-23 12:47:33 +0100

    deploy optisam app

---
+++ app.py
@@ -0,0 +1,102 @@
+import requests
+import hubspot
+from flask import Flask, request, jsonify
+import os
+from MAGIC_NUMBERS import *
+
+
+app = Flask(__name__)
+
+HUBSPOT_ACCESS_TOKEN = os.environ[HUBSPOT_TOKEN]
+client = hubspot.Client.create(access_token=HUBSPOT_ACCESS_TOKEN)
+hubspot_headers = {'Authorization': f'Bearer {HUBSPOT_ACCESS_TOKEN}'}
+
+
+def who_is_the_chef(contact_id):
+    endpoint_url = f"https://api.hubapi.com/crm/v4/objects/contacts/{contact_id}/associations/contacts"
+    response = requests.get(endpoint_url, headers=hubspot_headers)
+
+    if response.status_code != 200:
+        raise Exception(f"Failed to retrieve associations: {response.text}")
+
+    members = []
+    results = response.json().get('results', [])
+
+    for result in results:
+        contact_id_2 = result['toObjectId']
+        types = result['associationTypes']
+
+        for assoc in types:
+            if assoc['category'] == 'USER_DEFINED':
+                label = assoc['label']
+                ayant_droit = (contact_id_2, 'Chef de famille' if label != 'Ayant droit' else 'Ayant droit')
+                chef = (contact_id, 'Chef de famille' if label == 'Ayant droit' else 'Ayant droit')
+
+                if members:
+                    members.append(ayant_droit)
+                else:
+                    members.extend([chef, ayant_droit])
+
+    return members
+
+
+def get_associated_deals(contact_id):
+    endpoint_url = f"https://api.hubapi.com/crm/v3/objects/contacts/{contact_id}/associations/deals"
+    response = requests.get(endpoint_url, headers=hubspot_headers)
+    if response.status_code != 200:
+        raise Exception(f"Failed to retrieve deals: {response.text}")
+    return [deal['id'] for deal in response.json().get('results', [])]
+
+
+def get_deal_details(deal_id):
+    endpoint_url = f"https://api.hubapi.com/crm/v3/objects/deals/{deal_id}"
+    params = {
+        'properties': ','.join(PROPERTY_NAME)  # Request all properties
+    }
+    response = requests.get(endpoint_url, headers=hubspot_headers, params=params)
+    if response.status_code != 200:
+        raise Exception(f"Failed to retrieve deal details: {response.text}")
+
+    deal_data = response.json().get('properties', {})
+    if deal_data['pipeline'] == '256106182':
+        # Filter out properties that don't have a value or their value is 0
+        deal_details = {key: value for key, value in deal_data.items() if
+                        value is not None and (not isinstance(value, (int, float)) or value > 0)}
+    else:
+        deal_details = 'Pas de pipeline Vente Optique'
+
+    return deal_details
+
+
+def update_deal_property(deal_id, deal_details):
+    endpoint_url = f"https://api.hubapi.com/crm/v3/objects/deals/{deal_id}"
+    data = {
+        "properties": deal_details
+    }
+
+    response = requests.patch(endpoint_url, headers=hubspot_headers, json=data)
+    if response.status_code != 200:
+        raise Exception(f"Failed to update deal: {response.text}")
+
+
+@app.route('/hubspot_webhook', methods=['POST'])
+def main():
+    try:
+        contact_infos = request.json
+        contact_id_hubspot = contact_infos[VID]
+        labels = who_is_the_chef(contact_id_hubspot)
+        print(f"The chief is : {contact_id_hubspot}")
+        print(f"Common Association Labels: {labels}")
+        deals_id_chief = get_associated_deals(labels[0][0])
+        deals_details = get_deal_details(deals_id_chief[0])
+        print(deals_details)
+        for label in labels[1:]:
+            associated_contact = label[0]
+            deals_id_associated_contacts = get_associated_deals(associated_contact)
+            update_deal_property(deals_id_associated_contacts[0], deals_details)
+            #deals_details_associated_contacts = get_deal_details(deals_id_associated_contacts[0])
+            print(f'Contac ID Updated : {associated_contact}')
+            print(f'Deal ID Updated : {deals_id_associated_contacts[0]}')
+            print('-' * 100)
+    except Exception as e:
+        print(str(e))
--- magic_numbers.py
+++ magic_numbers.py
@@ -1,1 +1,24 @@
-INFO_ID = 'vid'
+HUBSPOT_TOKEN = 'HUBSPOT_TOKEN'
+
+PROPERTY_NAME = ['ancien_equipement_optique', 'ancien_equipement_optique_2', 'ancien_equipement_optique_3',
+                 'autre_mutuelle__', 'autre_mutuelle_____2', 'date_d_adhesion', 'date_d_adhesion___m1',
+                 'date_d_adhesion___m2', 'date_de_dernier_equipement',
+                 'date_dernier_equipement_lentilles', 'date_dernier_equipement_lentilles___m2',
+                 'date_dernier_equipement_lentilles___m3', 'date_dernier_equipement_lunettes',
+                 'date_dernier_equipement_lunettes___m2', 'date_dernier_equipement_lunettes___m3',
+                 'date_du_rendez_vous', 'date_fin___m2', 'date_fin___m3',
+                 'forfait_audio', 'forfait_lentilles', 'forfait_lentilles_2', 'forfait_lentilles_3', 'forfait_monture',
+                 'forfait_monture_2', 'forfait_monture_3', 'forfait_n_mutuelle_1', 'forfait_verres', 'forfait_verres_2',
+                 'forfait_verres_3', 'forfait_verres_progressifs', 'forfait_verres_progressifs_2',
+                 'forfait_verres_progressifs_3', 'garantie_mutuelle_1', 'identifiant_mutuelle_1',
+                 'informations_mutuelles__mdp_', 'mutuelles_suivies_la_meme_annee', 'nom_de_l_entreprise',
+                 'nom_de_l_entreprise_2', 'nom_de_l_entreprise_3',
+                 'nom_de_la_mutuelle', 'nom_de_la_mutuelle_2', 'nom_de_la_mutuelle_3', 'nom_de_la_mutuelle_concernee',
+                 'nombre_de_mutuelles',
+                 'options_du_contrat', 'options_du_contrat_2', 'options_du_contrat_3', 'type_de_mutuelle',
+                 'type_de_mutuelle_2', 'type_de_mutuelle_3',
+                 'type_de_remboursement',
+                 'type_rbsm_len_annee_en_cours__2023____m2', 'type_rbsm_lun_annee_en_cours__2023____m2',
+                 'dealstage', 'pipeline']
+
+VID = 'vid'
